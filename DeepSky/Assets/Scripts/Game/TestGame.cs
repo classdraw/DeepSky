db7ecd19944e778b32c42b882a1bca8e8c0105e8
jiayanyin <jiayanyin@ksztone.com>
2024-06-20 11:15:58 +0800

    音频测试注释

--- DeepSky/Assets/Scripts/Game/TestGame.cs
+++ DeepSky/Assets/Scripts/Game/TestGame.cs
@@ -34,32 +34,33 @@ public class TestGame : MonoBehaviour
     }
 
     void Update(){
-        if(Input.GetKeyDown(KeyCode.Space)){
-            GlobalEventListener.DispatchEvent(GlobalEventDefine.TestPlayerChange,new TestABC(){
-                id=123,speed=5.5f
-            });
-        }
+        // //测试脚本
+        // if(Input.GetKeyDown(KeyCode.Space)){
+        //     GlobalEventListener.DispatchEvent(GlobalEventDefine.TestPlayerChange,new TestABC(){
+        //         id=123,speed=5.5f
+        //     });
+        // }
 
-        if(Input.GetKeyDown(KeyCode.H)){
-            int rand=UnityEngine.Random.Range(1,6);
-            AudioManager.GetInstance().PlayAudioBG("audio_Action"+rand,-1f,2f,2f);
-        }else if(Input.GetKeyDown(KeyCode.J)){
+        // if(Input.GetKeyDown(KeyCode.H)){
+        //     int rand=UnityEngine.Random.Range(1,6);
+        //     AudioManager.GetInstance().PlayAudioBG("audio_Action"+rand,-1f,2f,2f);
+        // }else if(Input.GetKeyDown(KeyCode.J)){
 
-            AudioManager.GetInstance().PlayAudio2DEffect("audio_04_Fire_explosion_04_medium",false,(ss)=>{
-                XLogger.LogError("结束1");
-            });
-        }else if(Input.GetKeyDown(KeyCode.K)){
-            AudioManager.GetInstance().PlayAudio3DEffect("audio_04_Fire_explosion_04_medium",false,null,new Vector3(0,0,0),(ss)=>{
-                XLogger.LogError("结束3d11");
-            });
-            GameObject obj=new GameObject("3333");
+        //     AudioManager.GetInstance().PlayAudio2DEffect("audio_04_Fire_explosion_04_medium",false,(ss)=>{
+        //         XLogger.LogError("结束1");
+        //     });
+        // }else if(Input.GetKeyDown(KeyCode.K)){
+        //     AudioManager.GetInstance().PlayAudio3DEffect("audio_04_Fire_explosion_04_medium",false,null,new Vector3(0,0,0),(ss)=>{
+        //         XLogger.LogError("结束3d11");
+        //     });
+        //     GameObject obj=new GameObject("3333");
             
-            AudioManager.GetInstance().PlayAudio3DEffect("audio_04_Fire_explosion_04_medium",false,obj,new Vector3(0,0,0),(ss)=>{
-                XLogger.LogError("结束3d22");
-            });
-        }else if(Input.GetKeyDown(KeyCode.L)){
-            XEngine.Audio.AudioManager.Instance.PlayAudioUI("audio_DM-CGS-01",false,false);
-        }
+        //     AudioManager.GetInstance().PlayAudio3DEffect("audio_04_Fire_explosion_04_medium",false,obj,new Vector3(0,0,0),(ss)=>{
+        //         XLogger.LogError("结束3d22");
+        //     });
+        // }else if(Input.GetKeyDown(KeyCode.L)){
+        //     XEngine.Audio.AudioManager.Instance.PlayAudioUI("audio_DM-CGS-01",false,false);
+        // }
     }
 
 

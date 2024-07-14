e5e6a3cb1def4b39686f0231d2f34fef608c79a9
jiayanyin <365674221@qq.com>
2024-07-14 10:56:24 +0800

    服务器aoi测试脚本

---
+++ DeepSky/Assets/Scripts/Game/Test/TestManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XEngine.Net;
+public class TestManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_TestPrefab;
+    void Start(){
+
+    // #if UNITY_SERVER
+    if(NetManager.GetInstance().IsServer){
+        NetManager.GetInstance().SpawnObject(NetManager.ServerClientId,m_TestPrefab,Vector3.zero);
+    }
+    // #endif
+    }
+}

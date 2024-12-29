f4c4cb325ec239f66868193add704dc44307c06a
jiayanyin <365674221@qq.com>
2024-12-29 14:31:03 +0800

    遗漏

--- DeepSky/Assets/Scripts/Common/Config/ServerConfig.cs
+++ DeepSky/Assets/Scripts/Common/Config/ServerConfig.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="ServerConfig",menuName ="ScriptObjectCreate/ServerConfig",order =2)]
 public class ServerConfig:ScriptableObject
 {
+    [Header("玩家")]
     public Vector3 m_vPlayerDefaultPos;
 
     public void Init(){

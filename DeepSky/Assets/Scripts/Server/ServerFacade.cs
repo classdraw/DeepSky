69d20f9beb25391fcac9c93026d5d47439126746
jiayanyin <365674221@qq.com>
2025-01-05 12:42:52 +0800

    mongoDB

--- DeepSky/Assets/Scripts/Server/ServerFacade.cs
+++ DeepSky/Assets/Scripts/Server/ServerFacade.cs
@@ -7,8 +7,27 @@ using XEngine.Net;
 using XEngine.Utilities;
 using XEngine.Pool;
 using XEngine.Loader;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using MongoDB.Driver;
 
 namespace XEngine.Server{
+
+    //BsonId是主键 无论什么变量名 在mongodb都是_id
+    //BsonElement 要求一定序列化 即使private
+    //BsonIgnore忽视的变量
+    //[BsonDateTimeOptions(Kind =System.DateTimeKind.Local)]设置时区读取方式
+    public class TestUser{
+        [BsonId]
+        public int m_iUseId;
+        public string m_sName;
+
+        public int m_iAge;
+        [BsonDateTimeOptions(Kind =System.DateTimeKind.Local)]
+        public DateTime m_iTime;
+        // [BsonElement]
+        // private int m_iLv;
+    }
     public class ServerFacade:MonoBehaviour{
         private static ServerFacade m_kInstance;
         public static ServerFacade GetInstance(){
@@ -21,6 +40,46 @@ namespace XEngine.Server{
         private ServerGameSceneManager m_kServerGameSceneManager;
         private ResHandle m_ServerHandle;
         private ResHandle m_kServerResHandle;
+
+        private void TestConnectDB(){
+            string connectStr="mongodb://localhost:27017";
+            MongoClient mongoClient=new MongoClient(connectStr);
+            //没有自动创建
+            IMongoDatabase mongoDatabase=mongoClient.GetDatabase("MMONetCode");
+            //查找库建立集合
+            IMongoCollection<TestUser> userInfo=mongoDatabase.GetCollection<TestUser>("UserInfo");
+
+            //插入数据
+            TestUser testUser=new TestUser();
+            testUser.m_iAge=37;
+            testUser.m_iUseId=123;
+            testUser.m_sName="jiayanyin";
+            testUser.m_iTime=DateTime.Now;
+            userInfo.InsertOne(testUser);
+
+            //查询
+            var t=userInfo.Find(Builders<TestUser>.Filter.Eq("m_iUseId",123)).FirstOrDefault();
+            if(t!=null){
+                XLogger.LogError(">>>>"+t.m_sName);
+            }
+
+            List<TestUser> list=userInfo.Find(Builders<TestUser>.Filter.Empty).ToList();
+            if(list!=null){
+                XLogger.LogError(">>>>"+list.Count);
+            }
+            // userInfo.Find(Builders<TestUser>.Filter.And(Builders<TestUser>.Filter.Eq("m_iUseId",123),Builders<TestUser>.Filter.Gt("m_iAge",35))).FirstOrDefault();
+            //修改
+            var res=userInfo.UpdateOne(Builders<TestUser>.Filter.Eq("m_iUseId",123),Builders<TestUser>.Update.Set("m_iAge",100));
+            XLogger.LogError(">>>>"+res);
+
+            testUser.m_sName="jyy";
+            //替换
+            var res1=userInfo.ReplaceOne(Builders<TestUser>.Filter.Eq("m_iUseId",123),testUser);
+            XLogger.LogError(">>>>"+res1);
+
+            //删除
+            // userInfo.DeleteOne(Builders<TestUser>.Filter.Eq("m_iUseId",123));
+        }
 
         private void ClearServerCache(){
             if(m_kServerGameSceneManager!=null){
@@ -52,6 +111,7 @@ namespace XEngine.Server{
         }
 
         private void OnServerInit(object obj){
+            // TestConnectDB();
             InitServer();
             m_kServerGlobal.Init();
             m_kClientManager.Init();
